92a5241790a82539bb8bd5ea9fc8647c28d4d66e
Weltspear <>
2025-07-19 14:16:48 +0200

    Sources

---
+++ src/main/java/net/ticench/mod/TiCEnchMod.java
@@ -0,0 +1,49 @@
+package net.ticench.mod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TiCEnchMod.MODID)
+public class TiCEnchMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "ticench";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public TiCEnchMod(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.te.refmap.json");
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
+}
---
+++ src/main/java/net/ticench/mod/mixin/TinkerEnchantModifiableItem.java
@@ -0,0 +1,24 @@
+package net.ticench.mod.mixin;
+
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(slimeknights.tconstruct.library.tools.item.ModifiableItem.class)
+public class TinkerEnchantModifiableItem {
+
+    @Inject(at = @At("HEAD"), method = "isEnchantable", cancellable = true, remap = false)
+    public void isEnchantable(ItemStack stack, CallbackInfoReturnable<Boolean> cir){
+        cir.setReturnValue(true);
+        return;
+    }
+
+    @Inject(at = @At("HEAD"), method = "isBookEnchantable", cancellable = true, remap = false)
+    public void isBookEnchantable(ItemStack stack, ItemStack book, CallbackInfoReturnable<Boolean> cir){
+        cir.setReturnValue(true);
+        return;
+    }
+
+}
